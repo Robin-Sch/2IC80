9d156ffc4ce86ee6eea2b9c3ff5e7e12982984c8
Robin-Sch <46354497+Robin-Sch@users.noreply.github.com>
2026-01-13 18:46:11 +0100

    update readme to link to the report and explain branches

--- breaktooth/cmd/attacker/key_stroke_injector.py
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/python3
-
-from tools.utils.logs import logger
-from tools.emulator.kb_client import KbClient
-
-kb = KbClient()
-logger.success("Start up keyboard and you can inject")
-kb.event_loop()
--- breaktooth/cmd/generators/config.mk_generator.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-if [ "$#" -ne 1 ]; then
-    echo "Usage: $0 <replacement_string>"
-    exit 1
-fi
-replacement_string="$1"
-sed "s/template/$(echo $replacement_string | sed -e 's/[\/&]/\\&/g')/" ./tmpl/config.mk.tmpl > ./config.mk
-echo "config.mk file has been generated with TARGET_MAC_ADDRESS=$replacement_string"
